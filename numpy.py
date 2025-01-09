f62072b2714fa1e7a1c745cb1750c4e0a18f339d
Karthikbalusani-45 <karthikbalusani1@gmail.com>
2025-01-09 13:18:54 +0530

    Add files via upload

---
+++ numpy.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import my_module
+
+
+# In[4]:
+
+
+my_module.greet("Akshay")
+
+
+# In[3]:
+
+
+my_module.even([11,5,6,4,6,2])
+
+
+# In[2]:
+
+
+my_module.product(55,1,2)
+
+
+# In[5]:
+
+
+import numpy as np
+
+
+# In[6]:
+
+
+x = np.array([40,50,30,20])
+print(x)
+print(type(x))
+
+
+# In[10]:
+
+
+a = np.array([[5,23,42,9.2],[45,1,8,5.2]])
+print(a)
+print(type(a))
+print(a.shape)
+
+
+# In[14]:
+
+
+b = np.array([[1,'A',9.3],[4,'B',5.2]])
+print(b)
+print(b.dtype)
+print(b.shape)
+
+
+# In[ ]:
+
+
+
+
